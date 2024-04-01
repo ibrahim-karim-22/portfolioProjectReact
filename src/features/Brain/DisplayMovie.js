7046e2401ea91f770bb7454724a9afd6cd58b0ab
karim <hema_thechamp@yahoo.com>
2024-04-01 13:54:58 -0400

    fixed the movie display page layout still working on adding a favorite button at the bottom of the page

--- src/features/Brain/DisplayMovie.js
+++ src/features/Brain/DisplayMovie.js
@@ -1,18 +1,39 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { MoviesArr } from "../../app/assets/shared/MoviesMain";
-import { Container, Row, Col, CardBody, Card} from 'reactstrap'; 
+import { Container, Row, Col, CardBody, Card, CardTitle, CardText, CardSubtitle, Button } from 'reactstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { addToFavorites, removeFromFavorites } from '../../components/favorites/favoritesSlice';
+
 
 
 const DisplayMovie = () => {
     const { id } = useParams();
-
-  
     const movie = MoviesArr.find(movie => movie.id === parseInt(id));
+    const dispatch = useDispatch();
+    const currentUser = useSelector(state => state.user.currentUser);
+    const favoriteMovie = useSelector(state => state.favorites.faveList);
+    
+
+    const isFavorite = (movieId) => {
+        return favoriteMovie.some(movie => movie.id === movieId)
+    };
+
+    const handleFavoriteClick = (movie, type) => {
+        const payload = { item: movie, type };
+        if (isFavorite(movie.id)) {
+            dispatch(removeFromFavorites(payload));
+        } else {
+            dispatch(addToFavorites(payload));
+        }
+    };
+
 
     if (!movie) {
         return <div>Error</div>;
     }
+
+
 
     return (
         <Container>
@@ -23,33 +44,58 @@ const DisplayMovie = () => {
             </Row>
             <Row>
                 <Col>
-                    <video className='display-movie' width="100%" height="auto" controls>
+                    <video className='display-movie' width="70%" height="auto" controls>
                         <source src={movie.video} type="video/mp4" />
                         Your browser does not support this video type.
                     </video>
                 </Col>
             </Row>
             <Row>
-                <Col md='3'>
-                    <img className='movies-display-poster' src={movie.poster} alt={movie.title} />
-                </Col>
-                <Col md='3'>
-                    <p>Release Date: <br /> {movie.year} <br /> Plot: <br />{movie.plot}</p>
-                </Col>
-                <Col md='3'>
-                    <p>Trivia: <br />{movie.trivia}</p>
-                </Col>
-                <Col md='1'>
-                    <p>Director: {movie.director}</p>
-                </Col>
-                <Col md='1'>
-                    <p>Duration: {movie.duration}</p>
-                </Col>
-                <Col md='1'>
-                    <p>Cast: <br />{movie.cast.join(', ')}</p>
+                <Col>
+                    <Card className='card-main bg-black'>
+                        <CardBody>
+                            <Row>
+                                <Col lg='7'>
+                                    <img className='movies-display-poster mb-3' src={movie.poster} alt={movie.title} /> 
+                                </Col>
+                                <Col >
+                                    
+                                    <CardTitle tag="h1" className='text-warning'>
+                                {movie.name}
+                            </CardTitle>
+                            <CardSubtitle
+                                className="mb-2 text-light"
+                                tag="h3"
+                            >
+                                    {movie.year}
+                                   
+                            </CardSubtitle>
+                            <CardSubtitle className='text-muted' tag='h4'>
+                                 Director: <p className='text-light'>{movie.director}</p>
+                            </CardSubtitle>
+                            <CardSubtitle className='text-muted mt-1' tag='h4'>
+                            Cast: <p className='text-light'>{movie.cast.join(', ')}</p>
+                            </CardSubtitle>
+                            <CardText className='text-muted mt-3' tag='h4'>
+                               Plot: <p className='text-light'>{movie.plot}</p>
+                            </CardText>
+                            <CardText className='text-muted mt-1' tag='h4'>
+                              Trivia:  <p className='text-light'>{movie.trivia}</p> 
+                            </CardText>
+                            {/* {currentUser && (
+                                 <Button onClick={handleFavoriteClick(movie, 'movie')}>
+                                 {isFavorite ? "Remove from Favorites" : "Add to Favorites"}
+                             </Button>
+                             
+                            )} */}
+                           
+                                </Col>
+                            </Row>
+                           
+                        </CardBody>
+                    </Card>
                 </Col>
             </Row>
-       
         </Container>
     );
 };
